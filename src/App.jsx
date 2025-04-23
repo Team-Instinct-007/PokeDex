5f4c4b443f015bbe94f0b7176de01255561c600d
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-04-23 14:12:57 +0700

    create search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+  const [pokes, setPokes] = useState([])
+
+  useEffect(() => {
+    const debounce = setTimeout(async () => {
+      const searchPoke = async () => {
+        try {
+          if (search){
+            const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+            setPokes(data)
+            console.log(pokes)
+          } else {
+            const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+            setPokes(data)
+            console.log(pokes)
+          }
+        } catch (error) {
+          console.log(error)
+        } finally {
+          setTimeout(() => {}, 500)
+        }
+      }
+      searchPoke()
+      clearTimeout(debounce)
+    }, 500)
+  }, [search])
+  
 
   return (
-    <>
+    <div>
+      <form role='search' className="flex items-center w-full max-w-md mx-auto mt-4">
+        <input
+          type="search"
+          placeholder="Search Pokemon"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full px-4 py-2 text-gray-700 bg-white border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          aria-label='Search'
+        />
+      </form>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* {pokes?.map((poke) => {
+
+        })} */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
