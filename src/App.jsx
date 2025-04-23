9f423531450b6d33343c56cdf3f3b61d905c8536
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-04-24 00:26:03 +0700

    create object data

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,80 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [search, setSearch] = useState('')
-  const [pokes, setPokes] = useState([])
+  const [search, setSearch] = useState("");
+  const [pokes, setPokes] = useState([]);
+  const [completed, setCompleted] = useState([]);
 
   useEffect(() => {
     const debounce = setTimeout(async () => {
       const searchPoke = async () => {
         try {
-          const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+          const { data } = await axios.get(
+            "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+          );
           const allPokes = data.results;
           if (search) {
-            const filtered = allPokes.filter(poke =>
+            const filtered = allPokes.filter((poke) =>
               poke.name.toLowerCase().includes(search.toLowerCase())
             );
             setPokes(filtered);
-            console.log(filtered)
+
+            // isi dari filtered = array of object { name, url, [] }
+            // array of object { name, image, abilities, type, evolution}
+            setCompleted([]);
+
+            const detailedData = await Promise.all(
+              filtered.map(async (pokemon) => {
+                const { data } = await axios.get(
+                  `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+                );
+                return data;
+              })
+            );
+
+            setCompleted(detailedData);
+            console.log(detailedData);
           } else {
-            setPokes(allPokes); // default, kalau search kosong
-            console.log(allPokes)
+            setCompleted([]);
+
+            const detailedData = await Promise.all(
+              allPokes.map(async (pokemon) => {
+                const { data } = await axios.get(
+                  `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+                );
+                return data;
+              })
+            );
+
+            setCompleted(detailedData);
+            console.log(detailedData);
           }
         } catch (error) {
-          console.log(error)
-        } 
-      }
-      searchPoke()
-      
-    }, 500)
-    return () => clearTimeout(debounce)
-
-  }, [search])
-  
+          console.log(error);
+        }
+      };
+      searchPoke();
+    }, 500);
+    return () => clearTimeout(debounce);
+  }, [search]);
 
   return (
     <div>
-      <form role='search' className="flex items-center w-full max-w-md mx-auto mt-4">
+      <form
+        role="search"
+        className="flex items-center w-full max-w-md mx-auto mt-4"
+      >
         <input
           type="search"
           placeholder="Search Pokemon"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="w-full px-4 py-2 text-gray-700 bg-white border rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          aria-label='Search'
+          aria-label="Search"
         />
       </form>
 
@@ -55,7 +84,7 @@ function App() {
         })} */}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
