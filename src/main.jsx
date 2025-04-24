28b2f14c7d79a80da6a43221fef4123888862054
ziterz <ziady.mubaraq@gmail.com>
2025-04-24 17:06:35 +0700

    feat: integrate React Router for navigation and layout structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.jsx'
+import DetailPage from './pages/DetailPage.jsx'
+import FavoritePage from './pages/FavoritePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/favorite',
+        element: <FavoritePage />,
+      },
+      {
+        path: '/detail/:id',
+        element: <DetailPage />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
